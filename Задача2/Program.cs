63703f21826e03632cd5900291a3f221460573df
Katerina <m-a-c-u-k-a@mail.ru>
2022-12-06 18:19:12 +0700

    Точка пересечения прямых

---
+++ Задача2/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+double b1 = ReadInt("Введите координату b1: ");
+double k1 = ReadInt("Введите координату k1: ");
+double b2 = ReadInt("Введите координату b2: ");
+double k2 = ReadInt("Введите координату k2: ");
+
+double x = (b2-b1)/(k1-k2);
+double y = k1*x+b1;
+
+Console.WriteLine($"Точка пересечения двух прямых имеет координаты {x} и {y}");
+
+double ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
